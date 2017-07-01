0575f83f0f8b5201d2da911d217d08921c3d184f
Sanjay Purswani <sanjsanj@hotmail.com>
2017-07-01 10:34:40 +0100

    Refactor App to be stateless and test to use enzyme.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+/* global it, expect */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
