db3da9fd0f9db25536333319ae8597a704932a22
Sanjay Purswani <sanjsanj@hotmail.com>
2017-07-01 15:21:06 +0100

    Page has the right title

---
+++ e2etests/test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('TodoList App', () => {
+  it('Should load with the right title', () => {
+    browser.url('http://localhost:3000/');
+    const actualTitle = browser.getTitle();
+
+    expect(actualTitle).to.eql('Todo List');
+  });
+});
